73bcbadddc6748e85b91ce0499d0fabed3472ac1
Marc <marc.g.smith@gmail.com>
2019-03-21 20:53:55 +0000

    Unit and integration tests

--- src/integration-test/groovy/com/telleroo/BaseIntegrationSpec.groovy
+++ src/integration-test/groovy/com/telleroo/BaseIntegrationSpec.groovy
@@ -13,6 +13,6 @@ abstract class BaseIntegrationSpec extends Specification {
                 .withBlockTillRateLimitReset(true)
                 .withRequestsPerSecond(1000)
                 .withEndpoint(System.getProperty("tellerooEndpoint") ?: System.getenv("tellerooEndpoint") ?: "https://sandbox.telleroo.com")
-                .withApiKey(System.getProperty("tellerooApiKey") ?: System.getenv("tellerooApiKey") ?: "df29f8d55c70fdaaf6a987a1bd38ea4d3dd2beeee76ab2ce147cc574ec6e1bfea8af4f66d84a229df3ca097b56e8a20d022393ef78329fecf189e8475f2716ac"))
+                .withApiKey(System.getProperty("tellerooApiKey") ?: System.getenv("tellerooApiKey")))
     }
 }
